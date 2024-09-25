03869e17077397b3ee71e0ea3585dea0929fa6e9
equilibrium139 <zaidruwaishan@gmail.com>
2024-09-25 06:59:13 -0400

    thumbnail grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import logo from "./images/shitty_logo.webp";
 import './App.css'
 
 function Header() {
-  const [signedIn, setSignedIn] = useState(false);
   return (
     <header>
       <div>
@@ -27,10 +25,92 @@ function Header() {
   );
 }
 
+function ThumbnailGrid() {
+  return (
+    <div className="gridContainer">
+      {sampleItems.map((item, idx) => {
+       return ( 
+       <div className="gridItem">
+          <img className="thumbnail" src={item.thumbnail} alt={item.title}/>
+
+          <div>
+            <h4>{item.title}</h4>
+            <p>{item.description}</p>
+          </div>
+        </div> )
+      })}
+    </div>
+  )
+}
+
+const sampleItems = [
+  {
+    thumbnail: "thumbnail1.jpg",
+    title: "Asset 1",
+    description: "Description of Asset 1",
+  },
+  {
+    thumbnail: "thumbnail2.jpg",
+    title: "Asset 2",
+    description: "Description of Asset 2",
+  },
+  {
+    thumbnail: "thumbnail3.jfif",
+    title: "Asset 3",
+    description: "Description of Asset 3",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+  {
+    thumbnail: "thumbnail4.jfif",
+    title: "Asset 4",
+    description: "Description of Asset 4",
+  },
+];
+
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
+      <ThumbnailGrid />
     </div>
   );
 }
